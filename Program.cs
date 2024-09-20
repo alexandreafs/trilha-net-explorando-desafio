84668264490bd318a3d1292a7fd37aed4c40c225
alexandreafs <106822137+alexandreafs@users.noreply.github.com>
2024-09-19 22:39:03 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
-﻿using System.Text;
-using DesafioProjetoHospedagem.Models;
-
-Console.OutputEncoding = Encoding.UTF8;
 
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
+using Hospedagem.Models;
+
 List<Pessoa> hospedes = new List<Pessoa>();
 
 Pessoa p1 = new Pessoa(nome: "Hóspede 1");
@@ -16,7 +14,7 @@ hospedes.Add(p2);
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 10);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
